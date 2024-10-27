3e3352a8600892aaa38bef4d428c286b8d4f832e
Ayuub Mohamud <ayuub.mohamud@outlook.com>
2024-10-27 16:30:10 +0000

    changes

--- rtl/memorySystem/dcache.sv
+++ rtl/memorySystem/dcache.sv
@@ -45,7 +45,6 @@ module dcache #(parameter ACP_RS = 1) (
     output       logic [2:0]                    dcache_a_opcode,
     output       logic [2:0]                    dcache_a_param,
     output       logic [3:0]                    dcache_a_size,
-    output       logic [4:0]                    dcache_a_source,
     output       logic [31:0]                   dcache_a_address,
     output       logic [3:0]                    dcache_a_mask,
     output       logic [31:0]                   dcache_a_data,
@@ -56,7 +55,6 @@ module dcache #(parameter ACP_RS = 1) (
     input   wire logic [2:0]                    dcache_d_opcode,
     input   wire logic [1:0]                    dcache_d_param,
     input   wire logic [3:0]                    dcache_d_size,
-    input   wire logic [4:0]                    dcache_d_source,
     input   wire logic                          dcache_d_denied,
     /* verilator lint_on UNUSEDSIGNAL */
     input   wire logic [31:0]                   dcache_d_data,
